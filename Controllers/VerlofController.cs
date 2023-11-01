d2261749869ebab0b2f3a7ca4f13735e1f151cc5
Thijs Heerze <thijsheerze@gmail.com>
2023-11-01 15:28:33 +0100

    first commit

---
+++ Controllers/VerlofController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GeoProfs.Controllers
+{
+    public class VerlofController : Controller
+    {
+        public IActionResult Verlof()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Verlof.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace GeoProfs.Models
+{
+    public class Verlof
+    {
+        public int Id { get; set; }
+        public int WerknemerId { get; set; }
+        public string? RedenVerlof { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Datum { get; set; }
+    }
+}
