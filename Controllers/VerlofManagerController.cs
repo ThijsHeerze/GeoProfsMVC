0c1169752ad793b7651e66cb709e1a2095019e58
Ruben <0183263@student.roc-nijmegen.nl>
2023-11-03 13:53:50 +0100

    VerlofManager added

---
+++ Controllers/VerlofManagerController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeoProfs.Controllers {
+
+    public class VerlofManagerController : Controller
+    {
+        public IActionResult VerlofManager()
+        {
+            return View();
+        }
+    } 
+}
