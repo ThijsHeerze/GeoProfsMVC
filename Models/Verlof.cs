5cbc054fb0da88b07cc13c182867e2579485bb6a
Thijs Heerze <thijsheerze@gmail.com>
2023-11-10 14:21:05 +0100

    index edit

--- Models/Verlof.cs
+++ Models/Verlof.cs
@@ -1,40 +1,22 @@
 ﻿using GeoProfs.Models;
 using System.ComponentModel.DataAnnotations;
-namespace GeoProfs.Models
-{
+namespace GeoProfs.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
     public class Verlof
     {
         public int Id { get; set; }
         public int WerknemerId { get; set; }
         public int ManagerId { get; set; }
         public string? RedenVerlof { get; set; }
+        public int UrenGebruikt { get; set; }
+        public int UrenOver { get; set; }
+        public int Afdeling { get; set; }
 
-        public int VerlofurenGebruikt { get; set; }
-
-        public int VerlofurenOver { get; set; }
-
-        public int afdeling { get; set; }
-
+        [Display(Name = "Datum")]
         [DataType(DataType.Date)]
         public DateTime Datum { get; set; }
+
+        [Column(TypeName = "Naam")] 
+        public string Naam { get; set; }
     }
-
-    //gemaakt door milan verwijder maar als het fuck met shit
-    public class User
-    {
-        public string? Name { get; set; }
-        public string? Department { get; set; }
-        public bool Aanwezig { get; set; }
-
-        List<User> userList = new List<User>
-        {
-            new User { Name = "John Doe", Department = "HR", Aanwezig = true },
-            new User { Name = "Jane Smith", Department = "Finance", Aanwezig = false },
-            new User { Name = "Bob Johnson", Department = "IT", Aanwezig = true },
-            new User { Name = "Alice Brown", Department = "Marketing", Aanwezig = false },
-            new User { Name = "Charlie Davis", Department = "Engineering", Aanwezig = true }
-        };
-
-    }
-
-}
