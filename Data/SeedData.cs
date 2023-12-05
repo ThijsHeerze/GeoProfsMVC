32084806466a125fa7d578cc354b56905b7323db
Thijs Heerze <thijsheerze@gmail.com>
2023-12-05 12:07:03 +0100

    end of day commit

---
+++ Data/SeedData.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using GeoProfs.Data;
+using System;
+using System.Linq;
+
+namespace GeoProfs.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new GeoProfsContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<GeoProfsContext>>()))
+            {
+                // Look for any GeoProfs.
+                if (context.Verlof.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Verlof.AddRange(
+                    new Verlof
+                    {
+                        Naam = "Harry",
+                        Datum = DateTime.Parse("1989-2-12"),
+                        RedenVerlof = "Ziek",
+                        Afdeling = 1,
+                    },
+
+                    new Verlof
+                    {
+                        Naam = "Harry",
+                        Datum = DateTime.Parse("1989-2-12"),
+                        RedenVerlof = "Ziek",
+                        Afdeling = 1,
+                    },
+
+                    new Verlof
+                    {
+                        Naam = "Harry",
+                        Datum = DateTime.Parse("1989-2-12"),
+                        RedenVerlof = "Ziek",
+                        Afdeling = 1,
+                    },
+
+                    new Verlof
+                    {
+                        Naam = "Harry",
+                        Datum = DateTime.Parse("1989-2-12"),
+                        RedenVerlof = "Ziek",
+                        Afdeling = 1,
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- Models/Verlof.cs
+++ Models/Verlof.cs
@@ -7,16 +7,13 @@ using System.ComponentModel.DataAnnotations.Schema;
     {
         public int Id { get; set; }
         public int WerknemerId { get; set; }
-        public int ManagerId { get; set; }
+
+        [Column(TypeName = "Naam")]
+        public string? Naam { get; set; }
         public string? RedenVerlof { get; set; }
-        public int UrenGebruikt { get; set; }
-        public int UrenOver { get; set; }
         public int Afdeling { get; set; }
 
         [Display(Name = "Datum")]
         [DataType(DataType.Date)]
         public DateTime Datum { get; set; }
-
-        [Column(TypeName = "Naam")] 
-        public string Naam { get; set; }
     }
