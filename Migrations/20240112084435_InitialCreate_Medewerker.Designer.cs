e337b1ff0c382806cb9421e556b8e3caac563b2b
BigDickMilan <milantheunissen2003@gmail.com>
2024-01-22 10:39:39 +0100

    trek data vannuit FakeIdentity.cs

--- Migrations/20240112084435_InitialCreate_Medewerker.Designer.cs
+++ Migrations/20240112084435_InitialCreate_Medewerker.Designer.cs
@@ -4,6 +4,7 @@ using GeoProfs.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GeoProfs.Migrations
 {
     [DbContext(typeof(GeoProfsContext))]
-    partial class GeoProfsContextModelSnapshot : ModelSnapshot
+    [Migration("20240112084435_InitialCreate_Medewerker")]
+    partial class InitialCreate_Medewerker
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Models/FakeIdentity.cs
@@ -0,0 +1,25 @@
+﻿namespace GeoProfs.Models
+{
+    public class FakeIdentity
+    {
+        public class Medewerker
+        {
+            public int MedewerkerId { get; set; }
+            public string Naam { get; set; }
+            public string Functie { get; set; }
+        }
+
+        public List<Medewerker> GetMedewerkers()
+        {
+            List<Medewerker> medewerkers = new List<Medewerker>
+            {
+                new Medewerker { MedewerkerId = 10, Naam = "Jan", Functie = "Manager" },
+                new Medewerker { MedewerkerId = 11, Naam = "Piet", Functie = "Medewerker" },
+                new Medewerker { MedewerkerId = 12, Naam = "Klaas", Functie = "Schoonmaker" },
+                new Medewerker { MedewerkerId = 13, Naam = "Kees", Functie = "Schoonmaker" }
+            };
+
+            return medewerkers;
+        }
+    }
+}
