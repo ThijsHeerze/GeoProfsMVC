a1098d0704c996f895a76c5ad73610df70d8a155
BigDickMilan <milantheunissen2003@gmail.com>
2024-01-22 10:55:27 +0100

    changes

--- Models/FakeIdentity.cs
+++ Models/FakeIdentity.cs
@@ -16,7 +16,11 @@
                 new Medewerker { MedewerkerId = 10, Naam = "Jan", Functie = "Manager" },
                 new Medewerker { MedewerkerId = 11, Naam = "Piet", Functie = "Medewerker" },
                 new Medewerker { MedewerkerId = 12, Naam = "Klaas", Functie = "Schoonmaker" },
-                new Medewerker { MedewerkerId = 13, Naam = "Kees", Functie = "Schoonmaker" }
+                new Medewerker { MedewerkerId = 13, Naam = "Kees", Functie = "Schoonmaker" },
+                new Medewerker { MedewerkerId = 14, Naam = "freek", Functie = "Manager" },
+                new Medewerker { MedewerkerId = 15, Naam = "Joran", Functie = "Manager" },
+                new Medewerker { MedewerkerId = 16, Naam = "Leon", Functie = "Manager" },
+                new Medewerker { MedewerkerId = 17, Naam = "Milan", Functie = "Manager" }
             };
 
             return medewerkers;
